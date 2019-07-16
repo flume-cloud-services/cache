957aa103c39f408a0a5d89e347b03101794eef98
dimensi0n <erwan.roussel51@gmail.com>
2019-07-17 00:19:23 +0200

    Add auth

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/flume-cloud-services/cache
 
 go 1.12
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/flume-cloud-services/database v0.0.0-20190620230750-b58172f967f9
+)
--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
-import(
-	"fmt"
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/flume-cloud-services/cache/middleware"
+	"github.com/flume-cloud-services/database/controllers"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/signin", controllers.Signin)
+
+	http.Handle("/welcome", middleware.Middleware(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, _ = io.WriteString(w, "Welcome to you visitor !")
+		}),
+		middleware.AuthMiddleware,
+	))
+
+	log.Println("Starting server on port :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
